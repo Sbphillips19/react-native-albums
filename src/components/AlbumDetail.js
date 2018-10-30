52ecf48d3f658287400c6e9be8c9e483a9b732cf
sbphillips19 <sbphillips19@gmail.com>
2018-10-30 16:53:14 +0800

    albums added and scroll

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 class AlbumDetail extends Component {
   render() {
+    const { title, artist, thumbnail_image, image } = this.props.album;
+    const {
+      thumbnailStyle,
+      headerContentStyle,
+      tumbnailContainerStyle,
+      headerTextStyle,
+      imageStyle
+    } = styles;
+
     return (
       <Card>
         <CardSection>
-          <Text>{this.props.album.title}</Text>
+          <View style={tumbnailContainerStyle}>
+            <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+          </View>
+          <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>{title}</Text>
+            <Text>{artist}</Text>
+          </View>
+        </CardSection>
+        <CardSection>
+          <Image style={imageStyle} source={{ uri: image }} />
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  tumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
 
 export default AlbumDetail;
--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -3,20 +3,20 @@ import { View } from 'react-native';
 
 class CardSection extends Component {
   render() {
-    const styles = {
-      containerStyle: {
-        borderBottomWidth: 1,
-        padding: 5,
-        backgroundColor: '#fff',
-        justifyContent: 'flex-start',
-        flexDirection: 'row',
-        borderColor: '#ddd',
-        position: 'relative'
-      }
-    };
-
     return <View style={styles.containerStyle}>{this.props.children}</View>;
   }
 }
+
+const styles = {
+  containerStyle: {
+    borderBottomWidth: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative'
+  }
+};
 
 export default CardSection;
